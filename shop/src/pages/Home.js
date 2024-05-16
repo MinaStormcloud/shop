280c87e9c9d698634c75a01646ca6dbc7344f256
MinaStormcloud <164744381+MinaStormcloud@users.noreply.github.com>
2024-05-16 09:19:17 +0200

    W20

--- shop/src/pages/Home.js
+++ shop/src/pages/Home.js
@@ -2,13 +2,9 @@ import cakeImage from '../assets/images/joshua-glass.jpg';
 
 export default function Home({ children }) {
   return <>
-      
-
     <div className="profile-container">
         <h2>Welcome to Westside Belle Cakery!</h2>
-        <img src={cakeImage} alt="Cake" className="cake-image" /> 
-                       
+        <img src={cakeImage} alt="Cake" className="cake-image" />                        
     </div>    
   </>
-
 }
